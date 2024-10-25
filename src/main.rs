fd6a051337f099d969c2dce4f13ae37bc7729417
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-10-25 10:20:55 +0100

    very basic observer impl

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,72 @@
 trait Observable<T: Observer> {
+    fn set_message(&mut self, message: String);
+    fn new() -> Self;
     fn register(&mut self, observer: T);
     fn unregister(&mut self, observer: T);
     fn notify(&mut self);
 }
 
 trait Observer {
-    fn update(&self);
+    fn update(&self, message: String);
 }
 
+#[derive(PartialEq, Clone, Copy)]
 struct ObserverProcess {}
 
 struct Subject<T: Observer> {
+    message: String,
     observers: Vec<T>,
 }
 
 impl Observer for ObserverProcess {
-    fn update(&self) {
-        println!("update on me")
+    fn update(&self, message: String) {
+        println!("message: {message}");
     }
 }
 
-impl<T: Observer> Observable<T> for Subject<T> {
+impl<T: Observer + PartialEq> Observable<T> for Subject<T> {
+    fn new() -> Subject<T> {
+        Subject {
+            message: "".to_string(),
+            observers: vec![],
+        }
+    }
+
+    fn set_message(&mut self, message: String) {
+        self.message = message;
+        self.notify();
+    }
+
     fn register(&mut self, observer: T) {
-        todo!()
+        self.observers.push(observer);
     }
 
     fn unregister(&mut self, observer: T) {
-        todo!()
+        let index = self.observers.iter().position(|x| *x == observer).unwrap();
+        self.observers.remove(index);
     }
 
     fn notify(&mut self) {
-        todo!()
+        for observer in &self.observers {
+            observer.update(self.message.clone());
+        }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut subject = Subject::<ObserverProcess>::new();
+
+    let a = ObserverProcess {};
+    let b = ObserverProcess {};
+    let c = ObserverProcess {};
+
+    subject.register(a);
+    subject.register(b);
+    subject.register(c);
+
+    subject.set_message("Hello world".to_string());
+
+    subject.unregister(b);
+
+    subject.set_message("Bye world".to_string());
 }
